9b1a61d592aada9c1d6aae416694ae9b3e2c5e30
Davi15S <nguyenxd072@gmail.com>
2021-11-01 16:50:04 +0100

    Commit

--- 01-11-21/src/App.js
+++ 01-11-21/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { usesState, useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+const handleClick = () => {
+  console.log("User click");
+  setCount(count + 1);
+};
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Text</p>
+        <p>Hodnota počtu: {count}</p>
+        <div>
+          <button onClick={handleClick}>Přičti</button>
+        </div>
       </header>
     </div>
   );
